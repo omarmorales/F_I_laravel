aada0c6fcce07724477d7242b4d69602fa574561
omarsmoralesi <omorales@c-230.com>
2018-05-22 14:29:29 -0500

    highlight active navigation elements in nav

--- resources/views/_includes/nav/sidenav.blade.php
+++ resources/views/_includes/nav/sidenav.blade.php
@@ -2,21 +2,21 @@
   <aside class="menu m-t-30 m-l-10">
     <p class="menu-label">General</p>
     <ul class="menu-list">
-      <li><a href="{{ route('manage.dashboard') }}">Dashboard</a></li>
+      <li><a href="{{ route('manage.dashboard') }}" class="{{ Nav::isRoute('manage.dashboard') }}">Dashboard</a></li>
     </ul>
     <p class="menu-label">Content</p>
     <ul class="menu-list">
-      <li><a>Manage Staff</a></li>
-      <li><a>Manage Vacancies</a></li>
+      <li><a href="#" class="">Manage Staff</a></li>
+      <li><a href="#" class="">Manage Vacancies</a></li>
     </ul>
     <p class="menu-label">Administration</p>
     <ul class="menu-list">
-      <li><a href="#">Manage Users</a></li>
+      <li><a href="#" class="">Manage Users</a></li>
       <li>
         <a class="has-submenu">Roles &amp; Permissions</a>
         <ul class="submenu">
-          <li><a>Roles</a></li>
-          <li><a>Permissions</a></li>
+          <li><a href="#" class="">Roles</a></li>
+          <li><a href="#" class="">Permissions</a></li>
         </ul>
       </li>
     </ul>
