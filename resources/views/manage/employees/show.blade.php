6c4e58f24c0fed4d08fa3446e33453901517b2df
omarsmoralesi <omorales@c-230.com>
2018-06-25 20:22:50 -0500

    show staff is done

--- resources/views/manage/employees/show.blade.php
+++ resources/views/manage/employees/show.blade.php
@@ -9,76 +9,17 @@
         <li class="is-active"><a href="#" aria-current="page">{{ $employee->name }}</a></li>
       </ul>
     </nav>
-
-    <b-dropdown v-model="isPublic" class="m-b-10">
-      <button class="button is-primary" type="button" slot="trigger">
-        <template v-if="isPublic">
-          <i class="fas fa-globe m-r-5"></i>
-          <span class="m-r-5">Public</span>
-        </template>
-        <template v-else>
-          <i class="fas fa-lock m-r-5"></i>
-          <span class="m-r-5">Private</span>
-        </template>
-        <i class="fas fa-caret-down"></i>
-      </button>
-
-      <b-dropdown-item :value="true">
-        <div class="media">
-          <i class="fas fa-globe m-r-5 m-t-5"></i>
-          <div class="media-content">
-            <h3>Public</h3>
-            <small>Everyone can see</small>
-          </div>
-        </div>
-      </b-dropdown-item>
-
-      <b-dropdown-item :value="false">
-        <div class="media">
-          <i class="fas fa-lock m-r-5 m-t-5"></i>
-          <div class="media-content">
-            <h3>Private</h3>
-            <small>Only visible for admins</small>
-          </div>
-        </div>
-      </b-dropdown-item>
-    </b-dropdown>
-
     <div class="columns">
-      <div class="column is-4">
-        <div class="card">
-          <div class="card-image">
-            <figure class="image is-4by3">
-              <img src="{{ asset('storage/thumbnails/'.$employee->thumbnail) }}" alt="Placeholder image">
-            </figure>
-          </div>
-          <div class="card-content">
-            <div class="media">
-              <div class="media-content">
-                <p class="title is-4">{{ $employee->name }}</p>
-                <p class="subtitle is-6">{{ $employee->job_title }}</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
       <div class="column">
-        <div class="card">
-          <header class="card-header">
-            <p class="card-header-title">
-              Description
-            </p>
-          </header>
-          <div class="card-content">
-            <div class="content">
-              {{ $employee->description }}
-            </div>
+        <div class="profile">
+          <h1>{{ $employee->name }}</h1>
+          <div class="infoHolder">
+            <h2>{{ $employee->job_title }}</h2>
+            <span>{!! $employee->description !!}</span>
           </div>
-          <footer class="card-footer">
-            <a href="#" class="card-footer-item">Edit</a>
-            <a href="#" class="card-footer-item">Delete</a>
-          </footer>
+          <div class="imageHolder">
+            <img class="profilePic" src="{{ Storage::disk('spaces')->url('thumbnails/'.$employee->thumbnail) }}" alt="{{ $employee->name }}">
+          </div>
         </div>
       </div>
     </div>
@@ -92,7 +33,6 @@
     const app = new Vue({
       el: '#app',
       data: {
-        isPublic: {{ $employee->public }}
       }
     });
   }
