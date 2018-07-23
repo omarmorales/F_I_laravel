2fc98321299e1fa176a35096c7ba13c923d3bc5b
omarsmoralesi <omorales@c-230.com>
2018-07-23 18:50:05 -0500

    favicons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,11 +10,11 @@
 
   <title>{{ config('app.name', 'Laravel') }}</title>
 
-  <link rel="apple-touch-icon" sizes="152x152" href="{{ Storage::disk('spaces')->url('website_images/logos/apple-touch-icon.png') }}">
-  <link rel="icon" type="image/png" sizes="32x32" href="{{ Storage::disk('spaces')->url('website_images/logos/favicon-32x32.png') }}">
-  <link rel="icon" type="image/png" sizes="16x16" href="{{ Storage::disk('spaces')->url('website_images/logos/favicon-16x16.png') }}">
-  <link rel="manifest" href="{{ Storage::disk('spaces')->url('website_images/logos/site.webmanifest') }}">
-  <link rel="mask-icon" href="{{ Storage::disk('spaces')->url('website_images/logos/safari-pinned-tab.svg') }}" color="#5bbad5">
+  <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('images/apple-touch-icon.png') }}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon-32x32.png') }}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon-16x16.png') }}">
+  <link rel="manifest" href="{{ asset('images/site.webmanifest') }}">
+  <link rel="mask-icon" href="{{ asset('images/safari-pinned-tab.svg') }}" color="#5bbad5">
   <meta name="msapplication-TileColor" content="#da532c">
   <meta name="theme-color" content="#ffffff">
 
