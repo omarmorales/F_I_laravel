c3c1c1d7a61810ff1b621a54d0351bd85a5527c4
omarsmoralesi <omorales@c-230.com>
2018-09-27 16:29:05 -0500

    change layout for images

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -40,7 +40,9 @@
         </div>
         <div class="column is-5">
           <div class="fb-share-button is-pulled-right m-b-10 is-hidden-touch" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button" data-size="small" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
-          <img src="{{ asset('storage/thumbnails/'.$post->thumbnail) }}" alt="{{ $post->title_es }}">
+          <figure class="image is-square">
+            <img src="{{ asset('storage/thumbnails/'.$post->thumbnail) }}" alt="{{ $post->title_es }}">
+          </figure>
         </div>
       </div>
     </div>
